a8fbce6fa5824dae45c5ca010def74deb77644aa
Jsinco <jonah@jsinco.dev>
2026-05-29 23:00:46 -0400

    26.1 support and third party requests

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -18,26 +18,34 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+    implementation(project(":api"))
     implementation("org.bstats:bstats-bukkit:3.1.0")
+    implementation("space.arim.morepaperlib:morepaperlib:0.4.3")
+    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
+    // paper
+    implementation("net.kyori:adventure-platform-bukkit:4.4.1")
     implementation("net.kyori:adventure-api:4.25.0")
     implementation("net.kyori:adventure-text-minimessage:4.25.0")
     implementation("net.kyori:adventure-text-serializer-plain:4.25.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.4.1")
+    // paper
     compileOnly("com.google.code.gson:gson:2.10.1")
     compileOnly("net.momirealms:custom-nameplates:3.0.33")
-    implementation("space.arim.morepaperlib:morepaperlib:0.4.3")
-    implementation("org.aselstudios:LuxDialoguesAPI:3.0.5")
     compileOnly("net.Indyuce:MMOCore-API:1.12.1-SNAPSHOT")
     compileOnly("io.lumine:MythicLib-dist:1.7.1-SNAPSHOT")
-    compileOnly("com.github.retrooper:packetevents-spigot:2.11.1")
+    compileOnly("com.github.retrooper:packetevents-spigot:2.12.1")
     compileOnly("dev.aurelium:auraskills-api-bukkit:2.3.8")
     compileOnly("io.lumine:Mythic-Dist:5.9.5")
 }
 
 tasks.shadowJar {
     relocate("org.bstats", "org.aselstudios.luxdialogues.libs.bstats")
-    //relocate("net.kyori", "org.aselstudios.luxdialogues.libs.kyori")
     relocate("space.arim.morepaperlib", "org.aselstudios.luxdialogues.libs.morepaperlib")
+    //relocate("net.kyori", "org.aselstudios.luxdialogues.libs.kyori")
+    archiveBaseName.set("LuxDialogues")
+    archiveClassifier.set("")
+}
+
+tasks.jar {
+    enabled = false
 }
